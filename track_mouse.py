ff01d2fd70d71349dbc8b882a3eb028b613cce5b
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-15 17:01:04 +0530

    This is allright. Lets try with optical flow as well.

--- track_mouse.py
+++ track_mouse.py
@@ -25,6 +25,7 @@ trajectory_ = [ ]
 curr_loc_ = (100, 100)
 static_features_ = defaultdict( int )
 static_features_img_ = None
+distance_threshold_ = 200
 
 # global window with callback function
 window_ = "Mouse tracker"
@@ -177,13 +178,10 @@ def draw_point( frame, points, thickness = 2):
 
 def update_mouse_location( points, frame ):
     global curr_loc_
-    global static_features_
+    global static_features_img_
+    global distance_threshold_
+
     c0, r0 = curr_loc_ 
-
-    # All point should be close to mouse color.
-    mouseColor = np.mean( frame[c0-10:c0+10,r0-10:r0+10] )
-    print( 'mouse color %d' % mouseColor )
-
     res = {}
     newPoints = [ ]
     if points is None:
@@ -196,15 +194,14 @@ def update_mouse_location( points, frame ):
         x, y = int(x), int(y)
 
         # We don't want points which are far away from current location.
-        if distance( (x,y), curr_loc_ ) > 100:
+        if distance( (x,y), curr_loc_ ) > distance_threshold_:
+            print( 'd', end = '' )
             continue 
 
         # if this point is in one of static feature point, reject it
-        if static_features_[ (x,y) ] > 1:
-            print( 'x', end = '')
+        print( np.mean( static_features_img_ ) )
+        if static_features_img_[ y, x ] > 1:
             continue
-        if frame[x,y] > mouseColor + 20 or frame[x,y] < mouseColor - 20:
-            continue 
         newPoints.append( (x,y) )
         sumR += y
         sumC += x
@@ -228,13 +225,12 @@ def insert_int_corners( points ):
     drastically.
     """
     global static_features_img_
-    global static_features_
+    global distance_threshold_
     if points is None:
         return 
     for p in points:
         (x,y) = p.ravel()
-        static_features_[ (x,y) ] += 1
-        # static_features_img_[ y, x ] += 1
+        static_features_img_[ y, x ] += 1
 
 
 def track( cur ):
@@ -242,11 +238,8 @@ def track( cur ):
     global static_features_img_
     # Apply a good bilinear filter. This will smoothen the image but preserve
     # the edges.
-    cur = cv2.bilateralFilter( cur, 3, 50, 50 )
-    # p0 = cv2.goodFeaturesToTrack( cur, 200, 0.01, 5 )
-    p0 = cv2.cornerHarris( cur, 11, 7, 1 )
-    print( p0 )
-
+    cur = cv2.bilateralFilter( cur, 5, 50, 50 )
+    p0 = cv2.goodFeaturesToTrack( cur, 200, 0.1, 5 )
     insert_int_corners( p0 )
     draw_point( cur, p0, 1 )
 
@@ -309,6 +302,7 @@ def process( args ):
 
     print( '[INFO] FPS = %f' % fps )
     cur = fetch_a_good_frame( )
+
     static_features_img_ = np.zeros( cur.shape )
     # cur = threshold_frame( cur )
     while True:
@@ -326,6 +320,10 @@ def process( args ):
         assert cur.any()
         # cur = threshold_frame( cur )
         track( cur )
+
+        # After every 5 frame, Divide the static_features_img_.
+        if totalFramesDone % 5 == 0:
+            static_features_img_ = static_features_img_ / 5
 
 def main(args):
     # Extract video first
